e0370b4f2b0157503a75bdc39be844da84e02cea
Jonathan Lam <jlam55555@gmail.com>
2021-10-31 19:07:41 -0400

    updated powerset by adding case handling duplicates

--- power_set/power_set.cpp
+++ power_set/power_set.cpp
@@ -1,5 +1,6 @@
 #include "./power_set.hpp"
 
+// subsets i: lc_0078
 void subsets_rec(vvi& res, vi& stk, vi& nums, int i) {
         if (i == nums.size()) {
                 res.push_back(stk);
@@ -19,5 +20,44 @@ vvi subsets(vi& nums) {
         vvi res{};
         vi stk{};
         subsets_rec(res, stk, nums, 0);
+        return res;
+}
+
+// subsets i: lc_0090
+void subsets_rec_dup(vvi& res, vi& stk, vi& nums, vi& counts, int i) {        
+        if (i == nums.size()) {
+                res.push_back(stk);
+                return;
+        }
+        
+        // include repeated element from 0 to counts[i] times
+        for (int j = 0; j <= counts[i]; ++j) {
+                subsets_rec_dup(res, stk, nums, counts, i+1);
+                stk.push_back(nums[i]);
+        }
+        
+        for (int j = 0; j <= counts[i]; ++j) {
+                stk.pop_back();
+        }
+}
+    
+vvi subsets_dup(vi& nums) {
+        vvi res{};
+        vi stk{}, numsUniq{}, counts{};
+        int curPos = -1;
+        
+        sort(nums.begin(), nums.end());
+                
+        for (int i = 0; i < nums.size(); ++i) {
+                if (i && nums[i] == nums[i-1]) {
+                        ++counts[curPos];
+                } else {
+                        numsUniq.push_back(nums[i]);
+                        counts.push_back(1);
+                        ++curPos;
+                }
+        }
+        
+        subsets_rec_dup(res, stk, numsUniq, counts, 0);
         return res;
 }
--- power_set/power_set.hpp
+++ power_set/power_set.hpp
@@ -6,6 +6,11 @@
 typedef vector<int> vi;
 typedef vector<vi> vvi;
 
+// get power set of nums, which contains unique elements
 vvi subsets(vi& nums);
+
+// get power set of nums, which may contain duplicates
+// but don't return any duplicate elements
+vvi subsets_dup(vi& nums);
 
 #endif
