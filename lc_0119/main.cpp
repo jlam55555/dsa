5f2911cc016d16e74cf4b50137f079bdab25e844
Jonathan Lam <jlam55555@gmail.com>
2021-10-31 19:35:37 -0400

    condensed lc_0119

--- lc_0119/main.cpp
+++ lc_0119/main.cpp
@@ -3,13 +3,12 @@
 // don't ever write code like this, difficult to maintain and read
 
 vector<int> getRow(int ri) {
-        int rows[2][ri+1], r, i, j;
-        
-        for (r = i = 0, rows[0][0] = 1; i < ri; rows[!r][0] = rows[!r][i+1] = 1, ++i, r ^= 1)
-                for (j = 1; j <= i; ++j)
-                        rows[!r][j] = rows[r][j-1] + rows[r][j];
-        
-        return vector<int>(rows[r], rows[r]+ri+1);
+        int rs[2][ri+1], r, i, j;
+
+        for (r=i=0, rs[0][0]=1; i<ri; rs[!r][0]=rs[!r][i+1]=1, ++i, r^=1)
+                for (j=1; j<=i; rs[!r][j]=rs[r][j-1]+rs[r][j], ++j);
+
+        return vector<int>(rs[r], rs[r]+ri+1);
 }
 
 int main() {
